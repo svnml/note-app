f0010decb366382961b9311df62f013dcda2c7cd
Francisco Ruiz <fc.ruizd@gmail.com>
2025-02-10 22:51:59 -0300

    fix quotation

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -9,7 +9,13 @@ api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem(ACCES_TOKEN)
         if (token) {
-            config.headers.Authorization = 'Bearer ${token}'
+            config.headers.Authorization = `Bearer ${token}`
         }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
     }
 )
+
+export default api
